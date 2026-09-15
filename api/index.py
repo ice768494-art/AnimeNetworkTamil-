30765326b66a0fa39c038e8095e722003eb5c7d2
ice768494-art <ice768494@gmail.com>
2026-09-15 20:03:22 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,32 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
+
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        body = json.dumps({
+        response = {
             "status": "ok",
             "message": "Anime API is working!"
+        }
+
+        body = json.dumps(response).encode("utf-8")
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+
+        self.wfile.write(body)
+
+    def do_POST(self):
+        body = json.dumps({
+            "status": "ok"
         }).encode("utf-8")
 
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(body)))
         self.end_headers()
+
         self.wfile.write(body)
